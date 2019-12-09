dcf6693fa0252756fb7340eaa0463fa22fb8cbe2
Trista <Trista0711@gmail.com>
2019-12-09 16:05:21 +0800

    new

---
+++ TristasteFinal/Tristaste Website/map.js
@@ -0,0 +1,33 @@
+var mapboxTiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,});
+        var map = L.map('map')
+        .addLayer(mapboxTiles)
+        .setView([22.3193,114.1694], 10);
+
+
+
+var items = [];
+var airtable_read_endpoint = "https://api.airtable.com/v0/app240IewSUXfB0Q4/Map?api_key=keyqCeiJ2hCd2sEpn";
+
+var data = [];
+$.getJSON(airtable_read_endpoint, function(result) {
+    $.each(result.records, function(key,value) {
+        items = {};
+        items["Name"] = value.fields.Name;
+        items["Lat"] = value.fields.Lat;
+        items["Lng"] = value.fields.Lng;
+        items["img_url"] = value.fields.img_url;
+        data.push(items);
+            console.log(items);
+        });
+        
+        for (var i in data) {
+            var latlng = L.latLng({ lat: data[i].Lat, lng: data[i].Lng});
+            L.marker( latlng )
+            .bindPopup(data[i].Name + '<img src="' + data[i].img_url+'"width = "80px">')
+            .addTo(map);}
+        
+        
+        });
+
+ L.geoJson(marker).addTo(map);
---
+++ TristasteFinal/Tristaste Website/scriptlib2.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+    $("button#get_data").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appkggctHFt6xzWPZ/HealthyFood?api_key=keyqCeiJ2hCd2sEpn";
+        
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.Food);
+                       items.push(value.fields.Description);
+                       items.push(value.fields.Category);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+
+             $('#table1').DataTable( {
+                 data: dataSet,
+                 retrieve: true,
+                 columns: [
+                    { title: "Food",
+                      defaultContent:"" },
+                     { title: "Description",
+                         defaultContent:"" },
+                     { title: "Category",
+                       defaultContent:"" },
+                 ]
+             } );
+        }); // end .getJSON
+     }); // end button
+
+}); // document ready
